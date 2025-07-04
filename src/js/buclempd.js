8342ec8a28eb481be5fb58e6307d1527bc9a033e
Lucia <luciaqm2003@gmail.com>
2025-07-04 11:33:23 +0200

    Configuracion del reproductor de video completada

--- src/js/buclempd.js
+++ src/js/buclempd.js
@@ -1,25 +1,26 @@
-    const manifestUri = "../../backend/MPD/output/10.mpd"  
+const manifestUri = '../../Backend/MPD/output/video.mpd';  // La URL completa ya viene del servidor
 
-    // Mostrar el video
-    const video = document.getElementById("video");
-
-    // Instalar polyfills y reproducir con Shaka
-    shaka.polyfill.installAll();
-
-    if (!shaka.Player.isBrowserSupported()) {
-      alert("Navegador no soportado por Shaka Player");
-      return;
+    function  initApp(){
+        // Instalar polyfills y reproducir con Shaka
+        shaka.polyfill.installAll();
+        if (shaka.Player.isBrowserSupported()) {
+            initPlayer();
+        } else {
+            console.log("Navegador no soportado por Shaka Player");
+        }
     }
+    
+    document.addEventListener('DOMContentLoaded', initApp);
 
-    const player = new shaka.Player(video);
+    async function initPlayer() {
+        const video = document.getElementById("video");
+        const player = new shaka.Player(video);
 
-    try {
-      await player.load(manifestUri);
-      console.log('El video se ha cargado correctamente!');
-    } catch (error) {
-      console.error('Error al cargar el video:', error);
-      alert('Error al cargar el video: ' + error.message);
+        try {
+            await player.load(manifestUri);
+            console.log('El video se ha cargado correctamente!');
+        }catch (error) {
+            console.error('Error al cargar el video:', error);
+            alert('Error al cargar el video: ' + error.message);
+        }
     }
-
-
-  window.onload = cargarListaVideos;
